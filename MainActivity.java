7b5074060c4fab299aa7929bdfe20affd79d60c6
russdothtml <ryanrusselluy1204@gmail.com>
2025-08-04 23:46:32 +0800

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.wellnestlayoutjournal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton imagebutton = findViewById(R.id.createjournal_image_button);
+
+        imagebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, write_a_journal.class));
+            }
+        });
+
+    }
+}
